cf699c30601ebced6379276c7d5f2b5f480623d7
eduardomausa <mausaeduardo@gmail.com>
2022-04-28 07:59:55 -0300

    NodeJS Chapter II - APIs, Solid and Swagger

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,28 @@
+import { Router } from 'express';
+import multer from 'multer';
+
+import { v4 as uuidv4 } from 'uuid';
+import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { importCategoryControler } from '../modules/cars/useCases/importCategory';
+import { listCategoriesController } from '../modules/cars/useCases/listCategories';
+
+
+const categoriesRoutes = Router();
+
+const upload = multer({
+  dest: './tmp',
+});
+
+categoriesRoutes.post('/', (request, response) => {
+  return createCategoryController.handle(request, response);
+});
+
+categoriesRoutes.get('/', (request, response) => {
+  return listCategoriesController.handle(request, response);
+});
+
+categoriesRoutes.post('/import', upload.single('file'), (request, response) => {
+  return importCategoryControler.handle(request, response);
+})
+
+export { categoriesRoutes };
