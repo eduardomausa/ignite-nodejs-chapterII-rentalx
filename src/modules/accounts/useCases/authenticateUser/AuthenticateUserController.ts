0dfae5c33136ecf07e31f4cf4e481fea14a5bd67
eduardomausa <mausaeduardo@gmail.com>
2022-06-08 09:42:51 -0300

    Create update avatar use case and controller

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserController.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { container } from "tsyringe";
-import { AuthenticateUserUseCase } from "./authenticateUser/AuthenticateUserUseCase";
+import { AuthenticateUserUseCase } from "../authenticateUser/AuthenticateUserUseCase";
 
 
 class AuthenticateUserController {
